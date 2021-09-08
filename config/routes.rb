b3525960fa5c1766fa9cc0bbe4a37392a472d585
TaishiNishimura <taishi19880324@gmail.com>
2021-09-08 03:54:48 +0000

    [update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
