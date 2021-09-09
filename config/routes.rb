b2911571e335b899e1ed86a307d2512a0dcb09ce
TaishiNishimura <taishi19880324@gmail.com>
2021-09-09 15:34:43 +0000

    [relationship]error

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  resources :users, only: [:index, :edit, :update]
+
+  resources :users, only: [:index, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_025645) do
+ActiveRecord::Schema.define(version: 2021_09_09_141302) do
+
+  create_table "relationships", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "follower_id"
+    t.integer "followed_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
