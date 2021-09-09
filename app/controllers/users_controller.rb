ab9a07c4b6b930df387fa635d46d751a6398aba9
TaishiNishimura <taishi19880324@gmail.com>
2021-09-09 09:32:01 +0000

    [update]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,15 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to users_path(@user.id)
   end
-  
+
   private
 
   def user_params
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '01a5fef293c19b007355764d37aa6ba386de14e52da44d622f6e74a81c777088d21e36dab054ec991e0178fc2f3328e4a1c806ab46070954239cbd5d3f79772c'
