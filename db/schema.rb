d36a25a324b08d66a14096b82fecb01ac7a7bb0c
TaishiNishimura <taishi19880324@gmail.com>
2021-09-12 02:47:25 +0000

    [Add]column sns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_11_062101) do
+ActiveRecord::Schema.define(version: 2021_09_12_024456) do
 
   create_table "audios", force: :cascade do |t|
     t.string "audio"
@@ -38,6 +38,10 @@ ActiveRecord::Schema.define(version: 2021_09_11_062101) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
+    t.string "twitter"
+    t.string "facebook"
+    t.string "instagram"
+    t.string "youtube"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
