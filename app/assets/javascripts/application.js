e3dd3a20fdfb9d9a01fb0c6cda799d2e59cb27a5
TaishiNishimura <taishi19880324@gmail.com>
2021-09-13 10:58:49 +0000

    [update]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -70,13 +70,21 @@ $(function(){
 
   //ユーザーアイコンにマウスオーバーした時
   $(function(){
-    $('.dots-img').mouseover(function(){
-      $('.dots-img').addClass('play-btn');
+    $('.user-dots').mouseover(function(){
+      this.id
+      if (){
+      $('.user-dots').addClass('play-btn');
+      }
     });
-    $('.dots-img').mouseout(function(){
-      $('.dots-img').removeClass('play-btn');
+    $('.user-dots').mouseout(function(){
+      $('.user-dots').removeClass('play-btn');
     });
   });
+  
+  //引数を追加するid属性
+  //引数のid=ホバーしたidだったらaddclassする
+  
+  
 
 
   //ランダムに動くユーザーアイコン
