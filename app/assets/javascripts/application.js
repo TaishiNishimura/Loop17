914957d1234ef2414eb5f13d9e080db78c6803dd
TaishiNishimura <taishi19880324@gmail.com>
2021-09-19 02:55:45 +0000

    [update]about-bg

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -105,7 +105,6 @@ $(document).on('turbolinks:load', function() {
       var rot = Math.random() * 360;  // ランダムな角度
       var angle = rot * Math.PI / 180;
       user_icon[i].startTime = Date.now(); // 開始時間を覚える
-
       user_icon[i].vec = {    // 移動方向
           x: Math.cos(angle),
           y: Math.sin(angle)
@@ -113,7 +112,6 @@ $(document).on('turbolinks:load', function() {
     }
 
     var timer = setInterval(function() {
-
       for (var i = 0; i < user_icon.length;i++) {
         // 開始からの経過時間は？
         var timePassed = Date.now() - user_icon[i].startTime;
@@ -128,120 +126,16 @@ $(document).on('turbolinks:load', function() {
   function draw(obj,timePassed) {
     obj.style.top = timePassed / baseSpeed * obj.vec.y + 'px';
     obj.style.left = timePassed / baseSpeed * obj.vec.x + 'px';
-    // obj.style.top = Number(obj.style.top.slice(0,-2))+(baseSpeed * obj.vec.x) + 'px';
-    // obj.style.left = Number(obj.style.left.slice(0,-2))+(baseSpeed * obj.vec.y) + 'px';
-    // 画面外に出たら反対へ再配置
-    // console.log(document.getElementById(obj.id));
     if(obj.clientWidth+Number(obj.style.left.slice(0,-2)) > section1.clientWidth + 10) {
-        // obj.style.left = -5+"px";
-        // console.log(obj.style.left,1);
-        // document.getElementById(obj.id).vec.x *= -1;
         document.getElementById(obj.id).startTime = Date.now();
     } else if(obj.clientWidth+Number(obj.style.left.slice(0,-2)) < 0 - 10) {
-        // obj.style.left = section1.clientWidth + 5 + "px";
-        // console.log(obj.style.left,2);
-        // document.getElementById(obj.id).vec.x *= -1;
         document.getElementById(obj.id).startTime = Date.now();
     } else if(obj.clientHeight+Number(obj.style.top.slice(0,-2)) > section1.clientHeight + 10) {
-        // obj.style.top = -5+"px";
-        // console.log(obj.style.top,3);
-        // document.getElementById(obj.id).vec.y *= -1;
         document.getElementById(obj.id).startTime = Date.now();
     } else if(obj.clientHeight+Number(obj.style.top.slice(0,-2)) < 0 - 10) {
-        // obj.style.top = section1.clientHeight + 5+"px";
-        // console.log(obj.style.top,4);
-        // document.getElementById(obj.id).vec.y *= -1;
         document.getElementById(obj.id).startTime = Date.now();
     }
   }
-
-
-
-
-      var canvasWrap = document.querySelector('#canvas-wrap');
-      var canvas = document.querySelector('#canvas-container');
-      var ctx = canvas.getContext('2d');
-
-      var center = {};    // Canvas中央
-      var dots = [];      // パーティクル配列
-      var density = 17;  //パーティクルの数
-      var baseSize = 60;   // 大きさ
-
-      var Dot = function () {
-          this.size = Math.floor( Math.random() * 6 ) + baseSize; //大きさ
-
-          this.speed = this.size / baseSpeed; // 大きさによって速度変更
-          this.pos = {   // 位置
-              x: Math.random() * canvas.width,
-              y: Math.random() * canvas.height
-          };
-          var rot = Math.random() * 360;  // ランダムな角度
-          var angle = rot * Math.PI / 180;
-
-          this.vec = {    // 移動方向
-              x: Math.cos(angle) * this.speed,
-              y: Math.sin(angle) * this.speed
-          };
-      };
-      Dot.prototype = {
-          update: function() {
-              this.draw();
-
-              this.pos.x += this.vec.x;
-              this.pos.y += this.vec.y;
-
-              // 画面外に出たら反対へ再配置
-              if(this.pos.x > canvas.width + 10) {
-                  this.pos.x = -5;
-              } else if(this.pos.x < 0 - 10) {
-                  this.pos.x = canvas.width + 5;
-              } else if(this.pos.y > canvas.height + 10) {
-                  this.pos.y = -5;
-              } else if(this.pos.y < 0 - 10) {
-                  this.pos.y = canvas.height + 5;
-              }
-          },
-
-          draw: function() {
-              ctx.fillStyle = this.color;
-              ctx.beginPath();
-              ctx.arc(this.pos.x, this.pos.y, this.size, 0, 2 * Math.PI, false);
-              //ctx.drawImage(this, 0, 0);
-              ctx.fill();
-          }
-      };
-
-      function update() {
-          requestAnimFrame(update);
-          // 描画をクリアー
-          ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-          for (var i = 0; i < density; i++) {
-              dots[i].update();
-          }
-      }
-
-      function init() {
-          // canvasにコンテンツサイズをセット
-          canvas.setAttribute("width", canvasWrap.offsetWidth);
-          canvas.setAttribute("height", canvasWrap.offsetHeight);
-
-          // canvas中央をセット
-          center.x = canvas.width / 2;
-          center.y = canvas.height / 2;
-
-          // densityの数だけパーティクルを生成
-          for (var i = 0; i < density; i++) {
-            // console.log(new Dot());
-            //   var a = new Dot();
-            //   a.src = "/attachments/5c836abf34eec5af1156aaaac0730dcffe623615/store/fill/100/100/f1ad44702e745fd95cb26d185a6cb068b8ed16d2e9ce1a41bb56e378e962/profile_image.jpeg"
-              dots.push(new Dot());
-          }
-          update();
-      }
-      init();
-  }
-
 
 
   //ハンバーガーメニュー
@@ -326,3 +220,87 @@ $(document).on('turbolinks:load', function() {
       });
   });
 });
+
+
+
+
+      //アバウトページのパーティクル
+      var canvasWrap = document.querySelector('#canvas-wrap');
+      var canvas = document.querySelector('#canvas-container');
+      var ctx = canvas.getContext('2d');
+
+      var center = {};    // Canvas中央
+      var dots = [];      // パーティクル配列
+      var density = 17;  //パーティクルの数
+      var colors = ['#eeb900', '#6DD0A5', '#f799db'];
+      var baseSize = 60;   // 大きさ
+
+      var Dot = function() {
+          this.size = Math.floor( Math.random() * 6 ) + baseSize; //大きさ
+          this.color = colors[~~(Math.random() * 3)]; //色
+          this.speed = this.size / baseSpeed; // 大きさによって速度変更
+          this.pos = {   // 位置
+              x: Math.random() * canvas.width,
+              y: Math.random() * canvas.height
+          };
+          var rot = Math.random() * 360;  // ランダムな角度
+          var angle = rot * Math.PI / 180;
+
+          this.vec = {    // 移動方向
+              x: Math.cos(angle) * this.speed,
+              y: Math.sin(angle) * this.speed
+          };
+      };
+      Dot.prototype = {
+          update: function() {
+              this.draw();
+
+              this.pos.x += this.vec.x;
+              this.pos.y += this.vec.y;
+
+              // 画面外に出たら反対へ再配置
+              if(this.pos.x > canvas.width + 10) {
+                  this.pos.x = -5;
+              } else if(this.pos.x < 0 - 10) {
+                  this.pos.x = canvas.width + 5;
+              } else if(this.pos.y > canvas.height + 10) {
+                  this.pos.y = -5;
+              } else if(this.pos.y < 0 - 10) {
+                  this.pos.y = canvas.height + 5;
+              }
+          },
+
+          draw: function() {
+              ctx.fillStyle = this.color;
+              ctx.beginPath();
+              ctx.arc(this.pos.x, this.pos.y, this.size, 0, 2 * Math.PI, false);
+              ctx.fill();
+          }
+      };
+
+      function update() {
+          requestAnimFrame(update);
+          // 描画をクリアー
+
+          for (var i = 0; i < density; i++) {
+              dots[i].update();
+          }
+      }
+
+      function init() {
+          // canvasにコンテンツサイズをセット
+          canvas.setAttribute("width", canvasWrap.offsetWidth);
+          canvas.setAttribute("height", canvasWrap.offsetHeight);
+
+          // canvas中央をセット
+          center.x = canvas.width / 2;
+          center.y = canvas.height / 2;
+
+          // densityの数だけパーティクルを生成
+          for (var i = 0; i < density; i++) {
+              dots.push(new Dot());
+          }
+          update();
+      }
+      init();
+  }
