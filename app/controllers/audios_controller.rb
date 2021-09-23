e766169abf0d7e274301f96ba94b05b3ecea77cf
TaishiNishimura <taishi19880324@gmail.com>
2021-09-23 14:07:14 +0000

    [add]varidate

--- app/controllers/audios_controller.rb
+++ app/controllers/audios_controller.rb
@@ -6,12 +6,24 @@ class AudiosController < ApplicationController
   def create
     @audio = Audio.new(audio_params)
     @audio.user_id = current_user.id
+
+    unless under_17_seconds?(audio_params[:audio].tempfile)
+      redirect_to request.referer
+      return
+    end
+
     @audio.save
     redirect_to request.referer
   end
 
   def update
     @audio = Audio.where(user_id: current_user.id).first
+
+    unless under_17_seconds?(audio_params[:audio].tempfile)
+      redirect_to request.referer
+      return
+    end
+
     @audio.update(audio_params)
     redirect_to request.referer
   end
@@ -32,5 +44,13 @@ class AudiosController < ApplicationController
 
   def audio_params
     params.require(:audio).permit(:audio, :title, :genre_id)
+  end
+
+  def under_17_seconds?(file_path)
+      if Mp3Info.open(file_path).length.floor < 18
+        true
+      else
+        false
+      end
   end
 end
--- app/models/audio.rb
+++ app/models/audio.rb
@@ -3,14 +3,4 @@ class Audio < ApplicationRecord
   #belongs_to :genre_id
   mount_uploader :audio, AudiofileUploader
 
-  validate :audio_validate
-
-  def audio_validate
-    Mp3Info.open(current_path) do |mp3|
-      if mp3.length < 17
-        errors.add(:audio, "17秒以内のファイルにしてください")
-      end
-    end
-  end
-
 end
